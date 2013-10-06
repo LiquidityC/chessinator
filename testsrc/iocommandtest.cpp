ff3c0c4c484f287a3e609cecddd61157dc434cd3
Linus Probert <linus.probert@gmail.com>
2013-10-06 09:50:12 +0200

    Changed a faulty comment :P

--- testsrc/iocommandtest.cpp
+++ testsrc/iocommandtest.cpp
@@ -6,8 +6,8 @@ CPPUNIT_TEST_SUITE_REGISTRATION( IoCommandTest );
 void IoCommandTest::test_move()
 {
 	cengine::IoCommand move1("e3e4"); // Move
-	cengine::IoCommand move2("e3e4q"); // Castling
-	cengine::IoCommand not_a_move("accepted variants"); // Castling
+	cengine::IoCommand move2("e3e4q"); // Pawn promotion
+	cengine::IoCommand not_a_move("accepted variants");
 
 	CPPUNIT_ASSERT ( move1.is_move() );
 	CPPUNIT_ASSERT ( move2.is_move() );
