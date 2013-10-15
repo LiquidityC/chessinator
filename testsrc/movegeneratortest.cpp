dd92b3b19aa442e5bf5f0c43914330b2aff76c54
Johan Ask <johan.ask@netset.se>
2013-10-15 09:10:33 +0200

    Improved move tests with enemy pieces.

--- testsrc/movegeneratortest.cpp
+++ testsrc/movegeneratortest.cpp
@@ -16,7 +16,7 @@ void MoveGeneratorTest::test_move()
 	CPPUNIT_ASSERT(test_board("res/startboard.txt") == 20);
 	CPPUNIT_ASSERT(test_board("res/testcastling.txt") == 26);
 	CPPUNIT_ASSERT(test_board("res/testcastlingblock.txt") == 24);
-	CPPUNIT_ASSERT(test_board("res/testblackpieces.txt", false) == 58);
+	CPPUNIT_ASSERT(test_board("res/testblackpieces.txt", false) == 47);
 }
 
 int MoveGeneratorTest::test_board(std::string filename, bool whites_turn)
